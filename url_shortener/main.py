c3f7a6d2d3190c76e81da5ee29916121135c3064
gsak3l <gsakel5@gmail.com>
2020-11-06 19:56:24 +0200

    gui added and it looks really bad

--- url_shortener/main.py
+++ url_shortener/main.py
@@ -5,9 +5,10 @@ from tkinter import *
 
 # getting the large url, and making it short
 def urlShortener():
-    url_address_2 = url.get()  # getting the url
-    url_short = pyshorteners.Shortener().tinyurl.short(url_address_2)  # shorting the url
-    url_address.set(url_short)
+    url_address_2 = link.get()  # getting the link from the input
+    print(url_address_2)
+    short_url = pyshorteners.Shortener().tinyurl.short(url_address_2)  # making it short
+    url_address.set(short_url)
 
 
 # copying the link to the clipboard
@@ -16,15 +17,23 @@ def copy_url():
     pyperclip.copy(url_short)  # clipping
 
 
-# graphical user interface for the application
-root = Tk()  # calling the tk object
-root.geometry("400x200")  # dimensions
-root.title("URL SHORTENER")  # a title
-root.configure(bg="#49A")  # background color
-url = StringVar()
-url_address = StringVar()
+if __name__ == '__main__':
+    # graphical user interface for the application
+    root = Tk()  # calling the tk object
+    root.geometry("400x200")  # dimensions
+    root.title("URL SHORTENER")  # a title
+    root.configure(bg="#49A")  # background color
+    link = StringVar()
+    url_address = StringVar()
 
+    # adding elements ot the interface
+    Label(root, text="My URL Shortener", font="poppins").pack(pady=10)  # adding a title
+    Entry(root, textvariable="link").pack(pady=5)  # adding an input
+    Button(root, text="Generate Short URL", command=urlShortener).pack(pady=7)  # button that calls the urlShortener
+    Entry(root, textvariable=url_address).pack(pady=5)  # the short url output
+    Button(root, text="Copy URL", command=copy_url).pack(pady=5)  # button that copies the url
 
+    root.mainloop()
 
 # to install the libraries
 # pip install pyshorteners
